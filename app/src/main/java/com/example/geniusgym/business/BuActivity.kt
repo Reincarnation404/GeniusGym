71f334887902088a3cf183b1161ad22fb36e7d80
Tiv <rabbit881030@gmail.com>
2023-05-21 23:41:34 +0800

    Update: 新增通知頁面1/2

--- app/src/main/java/com/example/geniusgym/business/BuActivity.kt
+++ app/src/main/java/com/example/geniusgym/business/BuActivity.kt
@@ -3,6 +3,14 @@ package com.example.geniusgym.business
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
 import androidx.navigation.NavController
 import com.example.geniusgym.R
 import com.example.geniusgym.databinding.ActivityBuBinding
@@ -20,4 +28,6 @@ class BuActivity : AppCompatActivity() {
 
 
     }
+
+
 }
--- app/src/main/java/com/example/geniusgym/business/BuNotificationManageFragment.kt
+++ app/src/main/java/com/example/geniusgym/business/BuNotificationManageFragment.kt
@@ -2,10 +2,14 @@ package com.example.geniusgym.business
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
+import android.widget.Toolbar
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.Navigation
 import com.example.geniusgym.R
 import com.example.geniusgym.databinding.FragmentBuNotificaitonManageBinding
 
@@ -21,6 +25,45 @@ class BuNotificationManageFragment : Fragment() {
         binding = FragmentBuNotificaitonManageBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        with(binding){
+            setupMenu()
+            fabBuAddNotification.setOnClickListener {
+                Navigation.findNavController(it).navigate(R.id.action_buNotificaitonManage_to_buAddNotification)
+            }
+        }
+    }
+    private fun setupMenu() {
+        (requireActivity() as MenuHost).addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.bu_option_menu_filter_add_search, menu)
+
+            }
+
+
+            override fun onMenuItemSelected(item: MenuItem): Boolean {
+                // Validate and handle the selected menu item
+                when (item.itemId) {
+                    R.id.toolbar_search -> Toast.makeText(activity,"search", Toast.LENGTH_LONG).show()
+                    //R.id.toolbar_add -> Navigation.findNavController(it).navigate(R.id.action_buMenu_to_buMemberData)
+                    R.id.toolbar_filter -> {
+                        Toast.makeText(activity,"filter", Toast.LENGTH_LONG).show()
+                    }
+                }
+                // binding.viewModel?.text?.value = text
+                // 只要顯示選取項目文字可改成下列方式；但實際應用較複雜，需要上面when比對寫法
+                // binding.viewModel?.text?.value = item.title.toString()
+                // 做對應的處理即可回傳true，讓Menu處理到此為止
+                return true
+            }
+
+            override fun onPrepareMenu(menu: Menu) {
+                // Handle for example visibility of menu items
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
 
 
 }
