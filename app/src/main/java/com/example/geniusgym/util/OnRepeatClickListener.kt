e705e4c76fba367b8c74dd1afb9b958f35a98537
Brian <qazse753753@gmail.com>
2023-05-24 11:01:42 +0800

    Update: 重複點擊

---
+++ app/src/main/java/com/example/geniusgym/util/OnRepeatClickListener.kt
@@ -0,0 +1,16 @@
+package com.example.geniusgym.util
+
+import android.view.View
+import android.view.View.OnClickListener
+import kotlinx.coroutines.withContext
+
+class OnRepeatClickListener : OnClickListener {
+    override fun onClick(v: View?) {
+
+    }
+
+    private suspend fun delay() : Boolean{
+
+        return false
+    }
+}
