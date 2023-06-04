87323f98bccdbaa2c313f638321d067d147741e6
danny <a101010305@gmail.com>
2023-06-04 11:48:57 +0800

    update

--- app/src/main/java/com/example/geniusgym/member/controller/MePersonalTrain.kt
+++ app/src/main/java/com/example/geniusgym/member/controller/MePersonalTrain.kt
@@ -6,28 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.geniusgym.R
+import com.example.geniusgym.databinding.FragmentMePersonalTrainBinding
 import com.example.geniusgym.member.MePersonalTrainViewModel
 
 class MePersonalTrain : Fragment() {
-
-    companion object {
-        fun newInstance() = MePersonalTrain()
-    }
-
-    private lateinit var viewModel: MePersonalTrainViewModel
+    private lateinit var binding : FragmentMePersonalTrainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_me_personal_train, container, false)
+        val viewModel: MePersonalTrainViewModel by viewModels()
+        binding = FragmentMePersonalTrainBinding.inflate(inflater, container, false)
+        binding.viewmodel = viewModel
+        binding.lifecycleOwner = this
+        return binding.root
+         }
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MePersonalTrainViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
-}
