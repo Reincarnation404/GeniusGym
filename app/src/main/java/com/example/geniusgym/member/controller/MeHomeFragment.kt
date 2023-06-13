83af5bfe7dc948b6d4af4f2c1e7b2f1c25b16f0d
Brian <qazse753753@gmail.com>
2023-06-13 11:16:14 +0800

    Update: 完成首頁

--- app/src/main/java/com/example/geniusgym/member/controller/MeHomeFragment.kt
+++ app/src/main/java/com/example/geniusgym/member/controller/MeHomeFragment.kt
@@ -1,27 +1,42 @@
 package com.example.geniusgym.member.controller
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.geniusgym.R
+import com.example.geniusgym.databinding.FragmentMeHomeBinding
 import com.example.geniusgym.member.viewmodel.MeHomeViewModel
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.WriterException
+import com.journeyapps.barcodescanner.BarcodeEncoder
+
 
 class MeHomeFragment : Fragment() {
 
 
     private val viewModel: MeHomeViewModel by viewModels()
-
+    private lateinit var binding : FragmentMeHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_me_home, container, false)
+        binding = FragmentMeHomeBinding.inflate(LayoutInflater.from(requireContext()))
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val encoder = BarcodeEncoder()
+        try {
+            val bitmap = encoder.encodeBitmap("http://192.168.186.96:8080/THP101", BarcodeFormat.QR_CODE, 380, 380)
+            binding.ivMeHomeQRcode.setImageBitmap(bitmap)
+        }catch (e : WriterException){
+            e.printStackTrace()
+        }
+
     }
 }
