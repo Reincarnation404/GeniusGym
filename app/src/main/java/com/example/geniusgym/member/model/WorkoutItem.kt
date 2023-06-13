d97b70c6ea5d2bf7eed389fe8d932c4df751e226
Jeff <shian83320@gmail.com>
2023-06-13 15:02:18 +0800

    update:登入權限判定(公司端、教練端、會員端)

--- app/src/main/java/com/example/geniusgym/member/model/WorkoutItem.kt
+++ app/src/main/java/com/example/geniusgym/member/model/WorkoutItem.kt
@@ -2,12 +2,12 @@ package com.example.geniusgym.member.model
 import java.io.Serializable
 
 class WorkoutMainItem(
-    var sb_cat: String,
+    var sb_id: String,
     var sb_name: String
     )
 
 class WorkoutSmallItem(
-    var sb_cat: String,
+    var sb_id: String,
     var sc_id: String,
     var sc_name: String
     ):Serializable
