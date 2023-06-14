cef40752aefca69c267c316a74286eaf8819e238
Jeff <shian83320@gmail.com>
2023-06-14 23:57:03 +0800

    update:通知畫面(假資料未能顯示，待排除問題)

--- app/src/main/java/com/example/geniusgym/notification/NotificationViewModel.kt
+++ app/src/main/java/com/example/geniusgym/notification/NotificationViewModel.kt
@@ -1,7 +1,23 @@
 package com.example.geniusgym.notification
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.geniusgym.notification.model.Notification
 
 class NotificationViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val items: MutableLiveData<Notification> by lazy { MutableLiveData<Notification>() }
+    val item: MutableLiveData<List<Notification>> by lazy { MutableLiveData<List<Notification>>() }
+
+    init {
+        load()
+    }
+    private fun load() {
+        val notificationList: MutableList<Notification> = mutableListOf<Notification>()
+
+        notificationList.add(Notification(1,"m123456", 2, 600, "2023/06/16"))
+        notificationList.add(Notification(2, "m123456", 1, 300, "2023/06/15"))
+        notificationList.add(Notification(3, "m123456", 5, 1450, "2023/06/14"))
+
+        this.item.value = notificationList
+    }
 }
