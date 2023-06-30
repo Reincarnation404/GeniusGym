b69f7c2bb43e4094433e72271c68e007690c2da5
Brian <qazse753753@gmail.com>
2023-06-30 17:44:56 +0800

    Update: 修正bug，串後端api

--- app/src/main/java/com/example/geniusgym/factory/InstanceFactory.kt
+++
@@ -1,7 +0,0 @@
-package com.example.geniusgym.factory
-
-interface InstanceFactory {
-
-    fun craete() : Any
-
-}
--- app/src/main/java/com/example/geniusgym/factory/WebFactory.kt
+++
@@ -1,9 +0,0 @@
-package com.example.geniusgym.factory
-
-import com.example.geniusgym.util.WebRequestSpencer
-
-class WebFactory : InstanceFactory{
-    override fun craete(): Any {
-        return WebRequestSpencer()
-    }
-}
